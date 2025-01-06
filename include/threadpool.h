c8bf09e711d5acc1de1f507841ad515ce472d922
vu3.nguyen <vu3.nguyen@lge.com>
2025-01-06 12:54:30 +0900

    update

--- include/threadpool.h
+++ include/threadpool.h
@@ -1,10 +1,66 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
 
+#include <iostream>
+#include <queue>
+#include <chrono>
+#include <functional>
+#include <vector>
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+#include <atomic>
+
+struct Task
+{
+    std::chrono::steady_clock::time_point timestamp;
+    int priority;
+    std::function<void()> func;
+
+    bool operator<(const Task& other) const
+    {
+        if (timestamp == other.timestamp)
+        {
+            return priority < other.priority;
+        }
+
+        return timestamp > other.timestamp;
+    }
+};
+
 class ThreadPool
 {
 public:
+    explicit ThreadPool(std::size_t num_threads);
+    ~ThreadPool();
 
+    template <class F, class... Args>
+    void enqueue(int priority, F&& f, Args&&... args)
+    {
+        auto timestamp = std::chrono::steady_clock::now();
+        Task t = Task{
+            .timestamp = timestamp,
+            .priority = priority,
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        };
+
+        {
+            std::unique_lock<std::mutex> lock(m_mut_tasks);
+            m_tasks.push(t);
+        }
+
+        m_condition.notify_one();
+    }
+
+private:
+    void worker_thread_func();
+
+private:
+    std::atomic<bool> m_running;
+    std::vector<std::thread> m_workers;
+    std::priority_queue<Task> m_tasks;
+    std::mutex m_mut_tasks;
+    std::condition_variable m_condition;
 };
 
 #endif // THREAD_POOL_H
--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -1,1 +1,49 @@
 #include "threadpool.h"
+
+ThreadPool::ThreadPool(std::size_t num_threads)
+    : m_running(true)
+{
+    for (std::size_t i = 0U; i < num_threads; i++)
+    {
+        m_workers.emplace_back(&ThreadPool::worker_thread_func, this);
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    {
+        std::unique_lock<std::mutex> lock(m_mut_tasks);
+        m_running = false;
+    }
+
+    m_condition.notify_all();
+    for (std::thread& worker : m_workers)
+    {
+        worker.join();
+    }
+}
+
+void ThreadPool::worker_thread_func()
+{
+    while (true)
+    {
+        Task t;
+        {
+            std::unique_lock<std::mutex> lock(m_mut_tasks);
+            m_condition.wait(lock, [this]() { return !m_running || !m_tasks.empty(); });
+
+            if (!m_running)
+            {
+                break;
+            }
+
+            t = m_tasks.top();
+            m_tasks.pop();
+        }
+
+        if (t.func)
+        {
+            t.func();
+        }
+    }
+}
